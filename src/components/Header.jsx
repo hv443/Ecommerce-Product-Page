078cb2f82b8d7377f71097c51753e1263c43e3e7
unknown <vishwakarmaharsh443@gmail.com>
2022-11-11 02:02:46 +0530

    first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/logo/logo.svg";
 import cartIcon from "../assets/icons/icon-cart.svg";
 import closeIcon from "../assets/icons/icon-close.svg";
@@ -8,65 +8,50 @@ import avatarSvg from "../assets/avatar/image-avatar.png";
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [navLinksArray, setNavLinksArray] = useState([
+    const navLinksArray = [
         {
             name: "Home",
             path: "/",
-            isActive: false,
         },
         {
             name: "Collections",
             path: "/collection",
-            isActive: false,
         },
         {
             name: "Men",
             path: "/men",
-            isActive: false,
         },
         {
             name: "Women",
             path: "/women",
-            isActive: false,
         },
         {
             name: "About",
             path: "/about",
-            isActive: false,
         },
         {
             name: "Contact",
             path: "/contact",
-            isActive: false,
         },
-    ]);
+    ];
 
     function toggleMenu() {
         setIsMenuOpen((pre) => !pre);
     }
 
-    function activeLink(e) {
-        setIsMenuOpen((pre) => !pre);
-        const activeLink = navLinksArray.map((link) => {
-            if (e.target.innerText == link.name) {
-                return { ...link, isActive: true };
-            }
-            return { ...link, isActive: false };
-        });
-        setNavLinksArray(activeLink);
-    }
-
     const navLinks = navLinksArray.map((link, index) => {
         return (
-            <Link
-                onClick={activeLink}
+            <NavLink
+                onClick={toggleMenu}
                 key={index}
                 to={link.path}
-                className={`${
-                    link.isActive ? "border-element" : "border-transparent"
-                } border-b-2  hover:border-element duration-300 cursor-pointer block md:py-6`}>
+                className={({ isActive }) => {
+                    return isActive
+                        ? "border-element border-b-2 hover:border-element duration-300 cursor-pointer block md:py-6 "
+                        : "border-transparent border-b-2 hover:border-element duration-300 cursor-pointer block md:py-6";
+                }}>
                 {link.name}
-            </Link>
+            </NavLink>
         );
     });
 
@@ -76,9 +61,9 @@ const Header = () => {
                 <button className="md:hidden" onClick={toggleMenu}>
                     <img src={menuIcon} alt="menu-icon" />
                 </button>
-                <Link to="/">
+                <NavLink to="/">
                     <img src={logo} alt="logo" />
-                </Link>
+                </NavLink>
             </div>
             <nav
                 className={`${
